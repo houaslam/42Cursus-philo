80a47ab017dad9c30247efe496fc161abf962cc4
Hajar Ouaslam <houaslam@e2r10p11.1337.ma>
2023-02-28 11:53:52 +0100

    philo

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: houaslam <houaslam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:51:19 by houaslam          #+#    #+#             */
-/*   Updated: 2023/02/28 11:28:37 by houaslam         ###   ########.fr       */
+/*   Updated: 2023/02/28 11:53:48 by houaslam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,8 @@ void	initialize_data(t_data *data, char **av, int ac)
 	data->time_to_sleep = ft_atoi(av[4]);
 	if (ac == 6)
 		data->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
-
-		
 	data->philo = malloc(sizeof(t_philo) * data->philo_nb);
 	data->fork = malloc(sizeof(pthread_mutex_t) * data->philo_nb);
-
-	
 	while (i < data->philo_nb)
 	{
 		data->philo[i].nb = i + 1;
@@ -64,77 +60,48 @@ void	*routine(void	*ptr)
 	t_data	*data;
 
 	data = (t_data *)ptr;
-
-	// printf("++++%d\n", data->philo[data->i].nb);
-
-	// printf("----------------------------------|%d|----------------------------------", data->i);
-	// printf("check |%p|\n", data->philo[0].rf);
-	// printf("check |%p|\n", data->philo[1].rf);
-	// printf("check |%p|\n", data->philo[2].rf);
-	// printf("check |%p|\n", data->philo[3].rf);
-	// printf("check |%p|\n", data->philo[4].rf);
-	// printf("check |%p|\n", data->philo[5].rf);
-	// printf("check |%p|\n", data->philo[6].rf);
-	// printf("check |%p|\n", data->philo[7].rf);
-	int i = 0;
 	while (1)
 	{
-			pthread_mutex_lock(data->philo[data->i].lf);
-			pthread_mutex_lock(data->philo[data->i].rf);
-			
-			printf("%d took a fork\n", data->philo[data->i].nb);
-			usleep(data->time_to_eat);
-			printf("%d is eating\n", data->philo[data->i].nb);
-			slee9;
-			
-			pthread_mutex_unlock(data->philo[data->i].lf);
-			pthread_mutex_unlock(data->philo[data->i].rf);
-
-
-			time_to_last =;
-			eat ++;
-			
-			printf("%d is sleeping\n", data->philo[data->i].nb);
-			
-			
-			if (i == data->time_to_eat)
-				return NULL;
-			
-			usleep(data->time_to_sleep * 1000);
-			prit
-			printf("=====%d done\n", data->philo[data->i].nb);
+		printf("data.i = %d\n", data->i);
+		pthread_mutex_lock(data->philo[data->i].lf);
+		pthread_mutex_lock(data->philo[data->i].rf);
+		printf("%d took a fork\n", data->philo[data->i].nb);
+		usleep(data->time_to_eat * 1000);
+		printf("%d is eating\n", data->philo[data->i].nb);
+		pthread_mutex_unlock(data->philo[data->i].lf);
+		pthread_mutex_unlock(data->philo[data->i].rf);
+		data->meal_nb ++;
+		printf("%d is sleeping\n", data->philo[data->i].nb);
+		usleep(data->time_to_sleep * 1000);
+		printf("%d is thinking\n", data->philo[data->i].nb);
 	}
 	return (NULL);
 }
-
-# include <string.h>
 
 int	main(int ac, char **av)
 {
 	t_data		data;
 
-	
 	if (ac == 6 || ac == 5)
 	{
-		// memset(&data, 0, sizeof(t_data));÷
 		initialize_data(&data, av, ac);
 		philo_fork(&data);
 		printf("test |%d|\n", data.philo_nb);
 		data.i = 0;
-		while (data.i < data.philo_nb - 1)
+		while (data.i < data.philo_nb)
+		{	
 			pthread_create(&data.philo[data.i++].t, NULL, routine, &data);
-		
-		//pthread_mutex_lock(data.philo[6].lf);
-		
-		// unsigned long long o = 0;
-		while (1)
-		{
-			if (tmie to die)
-				print;
-			else if (argv == valide ==)
-				return ;
 		}
-		
+		// while (1)
+		// {
+		// 	if (time(data.c_time, data.e_time) >= data.time_to_die * 1000)
+		// 	{
+		// 		printf(" %d ms %d died\n", );
+				
+		// 	}
+		// 	else if (ac == 6 || data.meal_nb == )
+		// 		return ;
+		// }
 	}
 	else
 		write(2, "NUMBER OF ARGUMENTS IS INVALID\n", 31);
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: houaslam <houaslam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 10:43:36 by houaslam          #+#    #+#             */
-/*   Updated: 2023/02/27 18:17:02 by houaslam         ###   ########.fr       */
+/*   Updated: 2023/02/28 11:40:19 by houaslam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ typedef struct t_philo{
 }				t_philo;
 
 typedef struct t_data{
+	int					meal_nb;
 	int					i;
 	int					*tab;
 	int					philo_nb;
@@ -37,9 +38,9 @@ typedef struct t_data{
 	int					time_to_eat;
 	int					time_to_sleep;
 	int					number_of_times_each_philosopher_must_eat;
-	struct timeval		c_time;
 	t_philo				*philo;
 	pthread_mutex_t		*fork;
+	struct timeval		c_time;
 	struct timeval		e_time;
 }				t_data;
 
